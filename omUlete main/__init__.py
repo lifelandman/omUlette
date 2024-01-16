de891ba4f351f405c5c9ad22d8935fe3938bb47b
lifelandman <jacksonjams3@gmail.com>
2024-01-15 18:42:29 -0700

    fix sum extra polys, break sum else

--- omUlete main/__init__.py
+++ omUlete main/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "omUlete",
     "author": "Jackson \"Lifeland\" S.",
-    "version": (0, 8),
+    "version": (0, 9),
     "blender": (3, 5, 0),
     "description": "",
     "warning": "",
--- omUlete main/omuParse.py
+++ omUlete main/omuParse.py
@@ -1,7 +1,7 @@
 import bpy
 from . import omuAnims
 
-accepted_types = ('MESH')
+accepted_types = ('MESH', 'EMPTY')
 
 def process_mesh(mesh, name, mats, useTex, boneNames, vgroups, anim_check, boneDict, indent = 1):#should return egg string for this mesh without hierarchy indentation
     newliner = "\n" + (" "* indent)
@@ -9,41 +9,46 @@ def process_mesh(mesh, name, mats, useTex, boneNames, vgroups, anim_check, boneD
     
     #create vertecies.
     #We have several duplicate verticies so the same point can coorispond to multiple uv coordinates.
+    #TODO:: We have duplicates where they are not needed.
 
     loop_id_lookup = {}#This is gonna be a dictionary where a loop id coorisponds to one of the vertexpool's verticies.
                        #This is so when we're making polygons, we can look up verticie ids by loop, if need be.
 
-    uv_match_check = {}#ex of what this will look like: {vert.id, {(uvx, uvy), eggvertid}}
+    uv_match_check = {}#ex of what this will look like: {vertex_id: {uv_cor, eggvertid}}
     
     egg_data = newliner + "<VertexPool> " + name + "_pool {"
 
     idNum = 0
 
+    
     for loop in mesh.loops:#Generate verticies and lookup table(s)
         #Please note that in bpy loops refer to more or less the "triangles" of a mesh.
 
         vertex_id = loop.vertex_index
         loop_id = loop.index
         uvLoop = mesh.uv_layers[0]
-        uv_cor = uvLoop.uv[loop_id]
+        uv_cor = uvLoop.uv[loop_id].vector
+        uv_cor_str = str(uv_cor.x) +','+ str(uv_cor.y)#Stupid hack because something hasn't been working
+        
 
         vert = mesh.vertices[vertex_id]
 
         if vertex_id in uv_match_check:#TODO:: Test that this works
-            if uv_cor in uv_match_check[vertex_id]:
-                loop_id_lookup[loop_id] = uv_match_check[vertex_id][uv_cor]
+            if uv_cor_str in uv_match_check[vertex_id]:
+                loop_id_lookup[loop_id] = uv_match_check[vertex_id][uv_cor_str]
                 continue
-
-        egg_data += (newliner + " <Vertex> " + str(idNum) + ' { ' + str(vert.co[0]) + ' ' + str(vert.co[1]) + ' ' + str(vert.co[2])
+            
+        co = vert.undeformed_co
+        egg_data += (newliner + " <Vertex> " + str(idNum) + ' { ' + str(co[0]) + ' ' + str(co[1]) + ' ' + str(co[2])
         + newliner + "  <Normal> { " + str(vert.normal[0]) + ' ' + str(vert.normal[1]) + ' ' + str(vert.normal[2]) + '}' + newliner
-        + "  <UV> { " + str(uv_cor.vector.x) + ' ' + str(uv_cor.vector.y) + " }"
+        + "  <UV> { " + str(uv_cor.x) + ' ' + str(uv_cor.y) + " }"
         )
         egg_data += '}'
 
         if not vertex_id in uv_match_check:
             uv_match_check[vertex_id] = {}
 
-        uv_match_check[vertex_id][uv_cor] = vert
+        uv_match_check[vertex_id][uv_cor_str] = idNum
         loop_id_lookup[loop_id] = idNum
 
 
@@ -163,74 +168,76 @@ def childProcess(objects, known_objects, known_names, texture_path, using_anim, 
             known_names.append(name)
             ###################################
             
-            if obj.type in accepted_types:
 
-                egg_string += (newliner + "<Instance> %s {" % name + newliner)#Todo:: Use group where it's more appropriate.... likely best to do group creation on a per-object level
+            egg_string += (newliner + "<Instance> %s {" % name + newliner)#Todo:: Use group where it's more appropriate.... likely best to do group creation on a per-object level
 
-                #Apply Transforms
-                is_transformed = False
-                transform_string =" <Transform> {"
-                mat = obj.matrix_local#Hopefully the manual isn't lying and this really is parent-relative
-                transdata = obj.scale#scale cannot be determined from matrix alone if negative
-                if (transdata[0] != 0) or (transdata[1] != 0) or (transdata[2] != 0):
-                    transform_string += newliner + '  <Scale> { ' + str(transdata[0]) + ' ' + str(transdata[1]) + ' ' + str(transdata[2]) + ' }'
-                    is_transformed = True
+            #Apply Transforms
+            is_transformed = False
+            transform_string =" <Transform> {"
+            mat = obj.matrix_local#Hopefully the manual isn't lying and this really is parent-relative
+            transdata = obj.scale#scale cannot be determined from matrix alone if negative
+            if (transdata[0] != 0) or (transdata[1] != 0) or (transdata[2] != 0):
+                transform_string += newliner + '  <Scale> { ' + str(transdata[0]) + ' ' + str(transdata[1]) + ' ' + str(transdata[2]) + ' }'
+                is_transformed = True
 
-                transdata = mat.to_euler()
-                if (transdata[0] != 0):
-                    transform_string += newliner + '  <RotX> { ' + str(degrees(transdata[0])) + ' }'
-                    is_transformed = True
-                if (transdata[1] != 0):
-                    transform_string += newliner + '  <RotY> { ' + str(degrees(transdata[1])) + ' }'
-                    is_transformed = True
-                if (transdata[2] != 0):
-                    transform_string += newliner + '  <RotZ> { ' + str(degrees(transdata[2])) + ' }'
-                    is_transformed = True
+            transdata = mat.to_euler()
+            if (transdata[0] != 0):
+                transform_string += newliner + '  <RotX> { ' + str(degrees(transdata[0])) + ' }'
+                is_transformed = True
+            if (transdata[1] != 0):
+                transform_string += newliner + '  <RotY> { ' + str(degrees(transdata[1])) + ' }'
+                is_transformed = True
+            if (transdata[2] != 0):
+                transform_string += newliner + '  <RotZ> { ' + str(degrees(transdata[2])) + ' }'
+                is_transformed = True
 
-                transdata = mat.to_translation()
-                if (transdata[0] != 0) or (transdata[1] != 0) or (transdata[2] != 0):
-                    transform_string += newliner + '  <Translate> { ' + str(transdata[0]) + ' ' + str(transdata[1]) + ' ' + str(transdata[2]) + '}'
-                    is_transformed = True
-
-
-                if is_transformed:
-                    transform_string += newliner + '  }'
-                    egg_string += transform_string
-                del transdata
-                del transform_string
-                ##End transform application
+            transdata = mat.to_translation()
+            if (transdata[0] != 0) or (transdata[1] != 0) or (transdata[2] != 0):
+                transform_string += newliner + '  <Translate> { ' + str(transdata[0]) + ' ' + str(transdata[1]) + ' ' + str(transdata[2]) + '}'
+                is_transformed = True
 
 
-                child_addition = ''
-                children = obj.children
-                if len(children) > 0:
-                    child_addition = childProcess(children, known_objects, known_names, texture_path, using_anim, armDict, armMemDict, genDart, indent + 1)
-                if obj.type == "MESH":
+            if is_transformed:
+                transform_string += newliner + '  }'
+                egg_string += transform_string
+            del transdata
+            del transform_string
+            ##End transform application
 
-                    useTex = False
-                    #Add texture references, and calculate the slot data for materials
-                    picnum = len(obj.to_mesh().materials)
-                    if picnum != 0:
-                        for i in range(picnum):
-                            mat = obj.to_mesh().materials[i]
-                            img_name = None
-                            tex_name = None
-                            for x in mat.node_tree.nodes:
-                                if x.bl_static_type=='TEX_IMAGE':##THIS IS APPARENTLY DEPRICATED; and for some f*****g reason the only alternative I can find is as well. good luck, future me!
-                                   img_name = x.image.name
-                                   tex_name = img_name.replace(' ', '_')
-                                   useTex = True
-                                   mats.append(tex_name)
-                                   egg_string += "\n<Texture> " + tex_name + " { " + texture_path + img_name + " }"
-                                   break
 
-                        egg_string += newliner
+            child_addition = ''
+            children = obj.children
+            if len(children) > 0:
+                child_addition = childProcess(children, known_objects, known_names, texture_path, using_anim, armDict, armMemDict, genDart, indent + 1)
+            if obj.type == "MESH":
+
+                thisMesh = obj.to_mesh(preserve_all_data_layers=True, depsgraph= bpy.context.evaluated_depsgraph_get())
+                useTex = False
+                #Add texture references, and calculate the slot data for materials
+                picnum = len(thisMesh.materials)
+                if picnum != 0:
+                    for i in range(picnum):
+                        mat = thisMesh.materials[i]
+                        img_name = None
+                        tex_name = None
+                        for x in mat.node_tree.nodes:
+                            if x.bl_static_type=='TEX_IMAGE':##THIS IS APPARENTLY DEPRICATED; and for some f*****g reason the only alternative I can find is as well. good luck, future me!
+                                img_name = x.image.name
+                                tex_name = img_name.replace(' ', '_')
+                                useTex = True
+                                mats.append(tex_name)
+                                egg_string += "\n<Texture> " + tex_name + " { " + texture_path + img_name + " }"
+                                #TODO:: add alpha support
+                                break
+
+                    egg_string += newliner
 
                     
                     
                     #Process the mesh, and apply texture stuff in necissary
-                    new_addition = process_mesh(obj.to_mesh(), name, mats, useTex, boneNames, vgroups, anim_check, boneDict, indent + 1)
+                    new_addition = process_mesh(thisMesh, name, mats, useTex, boneNames, vgroups, anim_check, boneDict, indent + 1)
                     egg_string += new_addition
+                    obj.to_mesh_clear()
                 
                 egg_string += child_addition
                 
