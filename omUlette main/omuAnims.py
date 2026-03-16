92b4050d8e9067f0540244dabe7e528f04aed59a
lifelandman <jacksonjams3@gmail.com>
2026-03-16 12:37:37 -0600

    reset bone position before evaluating action

--- omUlette main/omuAnims.py
+++ omUlette main/omuAnims.py
@@ -212,6 +212,7 @@ def parse_anim_values(action, boneDict, armObj):
     aLen = action.curve_frame_range
     for bone in pose.bones:#Ensure all bones are selected
         bone.select = True
+        bone.matrix_basis.identity()
 
     for frame in range(int(aLen[0]), int(aLen[1])):
         pose.apply_pose_from_action(action, evaluation_time = frame)
